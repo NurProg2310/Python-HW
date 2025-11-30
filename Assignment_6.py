97d9265f64ae86bfdebfcbfc373b373ac0089ac6
NurProg2310 <153360973+NurProg2310@users.noreply.github.com>
2025-11-30 17:44:03 +0500

    add:Assignment_6

---
+++ Assignment_6.py
@@ -0,0 +1,78 @@
+a = 5
+b = 7
+print(a + b)
+
+s = "hello"
+print(s[::-1])
+
+s = "hello"
+print(len(s))
+
+s1 = "Hello "
+s2 = "World"
+print(s1 + s2)
+
+ch = "e"
+print(ch.lower() in "aeiou")
+
+s = "python"
+modified = s[-1] + s[1:-1] + s[0]
+print(modified)
+
+s = "hello world"
+print(s.upper())
+
+length = 5
+width = 3
+print(length * width)
+
+n = 10
+print(n % 2 == 0)
+
+s = "abcdef"
+print(s[:3])
+
+name = "Alice"
+age = 21
+print(f"My name is {name} and I am {age} years old.")
+
+s = "abcdefg"
+print(s[2:6])
+
+num_str = "123"
+num = int(num_str)
+print(num)
+s = "Hi"
+print(s * 3)
+
+a = 17
+b = 5
+print("Quotient:", a // b)
+print("Remainder:", a % b)
+
+a = 7
+b = 2
+print(a / b)
+
+s = "banana"
+print(s.count("a"))
+
+s = "He said \"Hello\" to me."
+print(s)
+
+s = """This is
+a multi-line
+string."""
+print(s)
+
+base = 3
+exp = 4
+print(base ** exp)
+
+s = "racecar"
+print(s == s[::-1])
+
+s1 = "Listen"
+s2 = "Silent"
+
+print(sorted(s1.lower()) == sorted(s2.lower()))
